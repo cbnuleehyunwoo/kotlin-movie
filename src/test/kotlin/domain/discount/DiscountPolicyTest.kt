84fe061a1784c118693152256914194c3294f67f
cbnuLeehyunwoo <henow123@naver.com>
2026-04-13 15:04:06 +0900

    test: DiscountPolicyTest 코드 가독성 개선 및 테스트 케이스 명확화

--- src/test/kotlin/domain/discount/DiscountPolicyTest.kt
+++ src/test/kotlin/domain/discount/DiscountPolicyTest.kt
@@ -9,37 +9,53 @@ import java.time.LocalDateTime
 class DiscountPolicyTest {
 
     @Test
-    fun `의도한 티켓 할인이 순서대로 적용되어 예상된 금액을 할인한다`() {
+    fun `의도한 티켓 할인이 순서대로 적용된다`() {
         // given
+        val ticketDiscountPolicy = TicketDiscountPolicy(
+            strategies = listOf(
+                MoviedayDiscount(),
+                TimeDiscount(),
+            )
+        )
         val given = Money(10_000)
         // when
-        val result = ticketDiscountPolicy.calculateDiscountResult(given, ticketContext)
+        val result = ticketDiscountPolicy.calculateDiscountResult(given, ticketDiscountContext)
+        val firstDiscount = MoviedayDiscount().apply(given, ticketDiscountContext)
+        val secondDiscount = TimeDiscount().apply(given , ticketDiscountContext)
+        val expected = given - (firstDiscount + secondDiscount)
         // then
-        assertThat(result).isEqualTo(Money(7000))
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `의도한 결제 할인이 순서대로 적용되어 예상된 금액을 반환한다`() {
+    fun `의도한 결제 할인이 순서대로 적용된다`() {
         // given
+        val totalDiscountPolicy = TotalDiscountPolicy(
+            strategies = listOf(
+                PaymentDiscount(),
+            )
+        )
         val given = Money(10_000)
         // when
-        val result = totalDiscountPolicy.calculateDiscountResult(given, paymentContext)
+        val result = totalDiscountPolicy.calculateDiscountResult(given, cardPaymentContext)
+        val firstDiscount = PaymentDiscount().apply(given, cardPaymentContext)
+        val expected = given - firstDiscount
         // then
-        assertThat(result).isEqualTo(Money(9500))
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `카드 결제 시 할인 금액을 반환한다`() {
+    fun `카드 결제 할인 시 5퍼센트의 할인 금액이 반환된다`() {
         // given
         val given = Money(10000)
         // when
-        val result = PaymentDiscount().apply(given, paymentContext)
+        val result = PaymentDiscount().apply(given, cardPaymentContext)
         // then
         assertThat(result).isEqualTo(Money(500))
     }
 
     @Test
-    fun `현금 결제 시 할인 금액을 반환한다`() {
+    fun `현금 결제 할인 시 2퍼센트의 할인 금액이 반환된다`() {
         // given
         val given = Money(10000)
         // when
@@ -49,43 +65,30 @@ class DiscountPolicyTest {
     }
 
     @Test
-    fun `무비데이 조건에 해당하는 경우 할인 금액을 반환한다`() {
+    fun `날짜가 무비데이 조건(10, 20, 30일)에 해당하는 경우 할인 금액(1000원)을 반환한다`() {
         // given
         val given = Money(10_000)
         // when
-        val result = MoviedayDiscount().apply(given, ticketContext)
+        val result = MoviedayDiscount().apply(given, ticketDiscountContext)
         // then
         assertThat(result).isEqualTo(Money(1000))
     }
 
     @Test
-    fun `특정 시간대 조건에 해당하는 경우 할인 금액을 반환한다`() {
+    fun `특정 시간대 조건(11시00분 이전, 20시00분 이후)에 해당하는 경우 할인 금액(2000원)을 반환한다`() {
         // given
         val given = Money(10_000)
         // when
-        val result = TimeDiscount().apply(given, ticketContext)
+        val result = TimeDiscount().apply(given, ticketDiscountContext)
         // then
         assertThat(result).isEqualTo(Money(2000))
     }
 
-    private val ticketDiscountPolicy = TicketDiscountPolicy(
-        strategies = listOf(
-            MoviedayDiscount(),
-            TimeDiscount(),
-        )
-    )
-
-    private val totalDiscountPolicy = TotalDiscountPolicy(
-        strategies = listOf(
-            PaymentDiscount(),
-        )
-    )
-
-    private val ticketContext = TicketDiscountContext(
+    private val ticketDiscountContext = TicketDiscountContext(
         dateTime = LocalDateTime.of(2026, 4, 10, 10, 0)
     )
 
-    private val paymentContext = PaymentDiscountContext(
+    private val cardPaymentContext = PaymentDiscountContext(
         paymentType = PaymentType.CREDIT_CARD
     )
 
