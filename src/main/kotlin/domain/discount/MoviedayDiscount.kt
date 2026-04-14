0b7c9504b3c3ea468aadb27a3a2bd6b8363eb9ac
cbnuLeehyunwoo <henow123@naver.com>
2026-04-14 21:06:13 +0900

    refactor: MoviedayDiscount 할인 금액 계산 로직에 오버로딩된 times 연산자를 사용하도록 수정

--- src/main/kotlin/domain/discount/MoviedayDiscount.kt
+++ src/main/kotlin/domain/discount/MoviedayDiscount.kt
@@ -5,7 +5,7 @@ import domain.common.Money
 class MoviedayDiscount : TicketDiscountStrategy {
     override fun apply(money: Money, context: TicketDiscountContext): Money {
         if (context.dateTime.toLocalDate().dayOfMonth % 10 == 0)
-            return Money((money.amount * 0.1).toInt())
+            return money * 0.1
         return Money(0)
     }
 }
