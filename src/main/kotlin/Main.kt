0c9c689331e77bf44e79e9970b21f76e05847e4d
cbnuLeehyunwoo <henow123@naver.com>
2026-04-15 01:58:22 +0900

    refactor: Main 코드 간소화 및 초기 데이터 생성 로직 분리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,40 @@
 import controller.Controller
-import domain.seat.Column
+import domain.common.TimeRange
 import domain.movie.Movie
-import domain.seat.Row
 import domain.movie.RunningTime
+import domain.movie.Title
 import domain.screening.Screening
 import domain.screening.ScreeningPeriod
 import domain.screening.ScreeningRoom
 import domain.screening.ScreeningRoomName
 import domain.screening.ScreeningSchedule
-import domain.seat.Seat
-import domain.seat.SeatPosition
-import domain.seat.Seats
-import domain.common.TimeRange
-import domain.movie.Title
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 
 fun main() {
-    Controller(
-        ScreeningSchedule(
-            screenings =
-                listOf(
-                    Screening(
-                        movie =
-                            Movie(
-                                title = Title("허닛"),
-                                runningTime = RunningTime(167),
-                                screeningPeriod =
-                                    ScreeningPeriod(
-                                        startDate = LocalDate.of(2026, 4, 8),
-                                        endDate = LocalDate.of(2026, 4, 9),
-                                    ),
-                            ),
-                        room =
-                            ScreeningRoom(
-                                name = ScreeningRoomName("커피"),
-                                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
-                            ),
-                        startTime = LocalDateTime.of(2026, 4, 8, 10, 0),
-                    ),
-                ),
-        ),
-    ).run()
+    val schedule = InitData.createSchedule()
+    Controller(schedule).run()
+}
+
+object InitData {
+    fun createSchedule(): ScreeningSchedule {
+        val movie = Movie(
+            title = Title("허닛"),
+            runningTime = RunningTime(167),
+            screeningPeriod = ScreeningPeriod(LocalDate.of(2026, 4, 8), LocalDate.of(2026, 4, 9))
+        )
+
+        val room = ScreeningRoom(
+            name = ScreeningRoomName("커피"),
+            operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0))
+        )
+
+        return ScreeningSchedule(
+            listOf(
+                Screening(movie = movie, room = room, startTime = LocalDateTime.of(2026, 4, 8, 10, 0)),
+                Screening(movie = movie, room = room, startTime = LocalDateTime.of(2026, 4, 8, 14, 0))
+            )
+        )
+    }
 }
